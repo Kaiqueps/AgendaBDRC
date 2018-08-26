5cd679483513dbc19350931c30cbfc5f3de9c7bd
Kaqueps <kaique.silva03@outlook.com>
2018-08-26 17:21:37 -0300

    On branch master

--- agenda.py
+++ agenda.py
@@ -2,13 +2,13 @@
 import funcoes
 
 funcoes.bemvindo()
+#funcoes.menu()
 
 #Opcoes do Usuario
-opcao = int(raw_input())
+opcao = int(input())
 print("Selecionou a ", opcao)
 
-
-#Estrutura de controle
+#Estrutura de controle/Aula: Seleção da solição
 if opcao == 1:
 	funcoes.adicionar()
 elif opcao == 2:
